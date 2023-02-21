fdd7716761f335734cd623cd4777f1a16af139ff
phucluu1609 <phuc.luu@jrgvn.com>
2023-02-21 17:36:18 +0700

    Fix UI Home Page

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,6 +1,11 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Container } from '@mui/material'
 import React from 'react'
 import { Helmet } from 'react-helmet'
+
+const ContainerStyle = {
+  height: '100vh',
+  paddingTop: 10,
+}
 
 export default function HomePage() {
   return (
@@ -9,9 +14,11 @@ export default function HomePage() {
         <title>STORE-CMS | Home</title>
         <meta name="description" content="Description of LoginPage" />
       </Helmet>
-      <Typography variant="h6" component="h1" color="inherit">
-        Home Page
-      </Typography>
+      <Container maxWidth="lg" sx={ContainerStyle}>
+        <Typography variant="h6" component="h1" color="inherit">
+          Home Page
+        </Typography>
+      </Container>
     </Box>
   )
 }
